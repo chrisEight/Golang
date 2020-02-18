d1d05d0e80e1c11b6dd7239deb329b7a5c11f9cd
chrisEight <chrisjaimon2012@gmail.com>
2020-02-18 20:47:53 +0530

    refresh

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
-package Golang
+package main
 
-import "rsc.io/quote"
+import "fmt"
 
-func Hello() string {
-	return quote.Hello()
+func main() {
+	fmt.Println("Hello World")
+	foo()
+	fmt.Println("foo printing is done")
+	for i := 0; i < 10; i++ {
+		if i%2 == 0 {
+			n, _ := fmt.Println(i)
+			fmt.Println(n, "\n")
+		}
+	}
+	bar()
+}
+func foo() {
+	fmt.Println("I'm in foo")
+}
+func bar() {
+	fmt.Println("BYEEEEEE BYEEEEE")
 }
